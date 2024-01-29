79f44df2da1050d486e3ce2fc07d97e3871089d8
IdaDanuartha <danuart21@gmail.com>
2024-01-29 23:56:30 +0800

    add model citizent

---
+++ app/Models/Citizent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\BloodGroup;
+use App\Enums\Gender;
+use App\Enums\MaritalStatus;
+use App\Enums\Religion;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class Citizent extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'birth_date' => 'date',
+        'gender' => Gender::class,
+        'blood_group' => BloodGroup::class,
+        'religion' => Religion::class,
+        'marital_status' => MaritalStatus::class
+    ];
+
+    public function user(): MorphOne
+	{
+		return $this->morphOne(User::class, 'authenticatable');
+	}
+
+    public function villageHead(): HasOne
+    {
+        return $this->hasOne(VillageHead::class);
+    }
+
+    public function environmentalHead(): HasOne
+    {
+        return $this->hasOne(EnvironmentalHead::class);
+    }
+
+    public function sectionHead(): HasOne
+    {
+        return $this->hasOne(SectionHead::class);
+    }  
+}
