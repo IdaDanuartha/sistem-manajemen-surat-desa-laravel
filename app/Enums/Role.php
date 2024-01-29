5110b218e5a60018be940f66eb17bcd2f67aaf5e
IdaDanuartha <danuart21@gmail.com>
2024-01-29 23:54:40 +0800

    add role enum

---
+++ app/Enums/Role.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Enums;
+
+enum Role: int
+{
+	case ADMIN = 1;
+	case VILLAGE_HEAD = 2;
+	case ENVIRONMENTAL_HEAD = 3;
+	case SECTION_HEAD = 4;
+	case CITIZENT = 5;
+
+	public function label()
+	{
+		return match ($this) {
+			self::ADMIN => 'Admin',
+			self::VILLAGE_HEAD => 'Kepala Kelurahan',
+			self::ENVIRONMENTAL_HEAD => 'Kepala Lingkungan',
+			self::SECTION_HEAD => 'Kepala Seksi',
+			self::CITIZENT => 'Warga',
+		};
+	}
+
+	public static function labels(): array
+	{
+		return [
+			2 => 'Kepala Kelurahan',
+			3 => 'Kepala Lingkungan',
+			4 => 'Kepala Seksi',
+			5 => 'Warga',
+		];
+	}
+}
