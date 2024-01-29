d48ab963b58a609aa9795e399688751469c0111b
IdaDanuartha <danuart21@gmail.com>
2024-01-29 23:57:07 +0800

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
+use App\Enums\UserStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -17,11 +21,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -39,7 +39,58 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'role' => Role::class,
+        'status' => UserStatus::class
     ];
+
+    public function isAdmin(): bool
+    {
+        return $this->role === Role::ADMIN;
+    }
+
+    public function isVillageHead(): bool
+    {
+        return $this->role === Role::VILLAGE_HEAD;
+    }
+
+    public function isEnvironmentalHead(): bool
+    {
+        return $this->role === Role::ENVIRONMENTAL_HEAD;
+    }
+
+    public function isSectionHead(): bool
+    {
+        return $this->role === Role::SECTION_HEAD;
+    }
+
+    public function isCitizent(): bool
+    {
+        return $this->role === Role::CITIZENT;
+    }
+
+    public function disable(): bool
+    {
+        return $this->update(['role' => UserStatus::NONACTIVE]);
+    }
+
+    public function activate(): bool
+    {
+        return $this->update(['role' => UserStatus::ACTIVE]);
+    }
+
+    public function disabled(): bool
+    {
+        return $this->status === UserStatus::NONACTIVE;
+    }
+
+    public function isActive(): bool
+    {
+        return !$this->disabled();
+    }
+
+    public function authenticatable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
