db954f3fced8fab7c9a3577af0539f86d99c5308
IdaDanuartha <danuart21@gmail.com>
2024-01-29 19:10:04 +0800

    add assets

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard.analytics.index');
+})->name('dashboard');
+
+Route::get('dashboard/profile', function() {
+    return true;
+})->name('profile.index');
+
+Route::get('auth/logout', function() {
+    return true;
+})->name('logout');
