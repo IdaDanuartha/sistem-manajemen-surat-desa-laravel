cc4f0f734bd0e50211fe22b6938354088242b77b
IdaDanuartha <danuart21@gmail.com>
2024-01-30 17:16:09 +0800

    add middleware is citizent

---
+++ app/Http/Middleware/IsCitizent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsCitizent
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(auth()->user()->role === Role::CITIZENT) return $next($request);
+        return abort(404);
+    }
+}
