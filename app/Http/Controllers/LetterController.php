4ffeb3768ebc11a03115380ffa0c3fe1d82a5de5
IdaDanuartha <danuart21@gmail.com>
2024-01-30 17:14:53 +0800

    update code letter controller

--- app/Http/Controllers/LetterController.php
+++ app/Http/Controllers/LetterController.php
@@ -2,64 +2,132 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Role;
+use App\Http\Requests\Letter\StoreLetterRequest;
+use App\Http\Requests\Letter\UpdateLetterRequest;
 use App\Models\Letter;
+use App\Repositories\LetterRepository;
+use App\Utils\ResponseMessage;
+use Exception;
 use Illuminate\Http\Request;
 
 class LetterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function __construct(
+        protected readonly LetterRepository $letterRepository,
+        protected readonly ResponseMessage $responseMessage
+    ) {}
+
+    public function index(Request $request)
+    {     
+        if(auth()->user()->role === Role::VILLAGE_HEAD) {
+            $letters = $this->letterRepository->findLetterByVillageHead();
+        } else if(auth()->user()->role === Role::SECTION_HEAD) {
+            $letters = $this->letterRepository->findLetterBySectionHead();
+        } else if(auth()->user()->role === Role::CITIZENT) {
+            $letters = $this->letterRepository->findLetterByCitizent();
+        } else {
+            $letters = $this->letterRepository->findAll();
+        }
+        return view('dashboard.letters.index', compact('letters'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
-    {
-        //
+    {                                           
+        return auth()->user()->role === Role::CITIZENT ? 
+               view('dashboard.letters.crud.create') : 
+               abort(404);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Letter $letter)
-    {
-        //
+    {                                                   
+        $get_letter = $this->letterRepository->findById($letter);
+        return view('dashboard.letters.crud.detail', compact('get_letter'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Letter $letter)
-    {
-        //
+    {                                           
+        return view('dashboard.letters.crud.edit', compact('letter'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Letter $letter)
-    {
-        //
+    public function store(StoreLetterRequest $request)
+    {            
+        if(auth()->user()->role === Role::CITIZENT) {
+            try {            
+                $store = $this->letterRepository->store($request->validated());            
+    
+                if($store instanceof Letter) return redirect(route("letters.index"))
+                                    ->with("success", $this->responseMessage->response('Surat'));
+    
+                throw new Exception;
+            } catch (\Exception $e) {  
+                logger($e->getMessage());
+    
+                return redirect(route("letters.create"))->with("error", $this->responseMessage->response('surat', false));
+            }
+        } else {
+            abort(404);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    public function update(UpdateLetterRequest $request, Letter $letter)
+    {
+        try {                     
+            $update = $this->letterRepository->update($request->validated(), $letter);
+
+            if($update == true) {
+                return redirect(route('letters.index'))
+                                ->with('success', $this->responseMessage->response('Surat', true, 'update'));
+            } else if(isset($update["status"])) {
+                return redirect()->route('letters.index')->with('error', $update["message"]);
+            }
+
+            throw new Exception;
+        } catch (\Exception $e) {
+            return redirect()->route('letters.edit', $letter->id)->with('error', $this->responseMessage->response('surat', false, 'update'));
+        }
+    }
+
+    public function addSignatureToLetter(Request $request, Letter $letter)
+    {
+        try {                     
+            $update = $this->letterRepository->addSignature($request->signature_image, $letter);
+
+            if($update) return redirect(route('letters.show', $letter->id))
+                                ->with('success', $this->responseMessage->response('Surat', true, 'update'));            
+            throw new Exception;
+        } catch (\Exception $e) {
+            return redirect()->route('letters.show', $letter->id)->with('error', $this->responseMessage->response('surat', false, 'update'));
+        }
+    }
+
+    public function approveLetter(Request $request, Letter $letter)
+    {
+        try {                     
+            $update = $this->letterRepository->updateLetterStatus($letter);
+
+            if($update) return redirect(route('letters.show', $letter->id))
+                                ->with('success', $this->responseMessage->response('Surat', true, 'update'));            
+            throw new Exception;
+        } catch (\Exception $e) {
+            return redirect()->route('letters.show', $letter->id)->with('error', $this->responseMessage->response('surat', false, 'update'));
+        }
+    }
+
     public function destroy(Letter $letter)
     {
-        //
+        try {
+            $delete = $this->letterRepository->delete($letter);  
+
+            if(!isset($delete["status"])) {
+                return redirect()->route('letters.index')->with('success', $this->responseMessage->response('Surat', true, 'delete'));
+            } else if(isset($delete["status"])) {
+                return redirect()->route('letters.index')->with('error', $delete["message"]);
+            }
+
+            throw new Exception;
+        } catch (\Exception $e) {            
+            return redirect()->route('letters.index')->with('error', $this->responseMessage->response('surat', false, 'delete'));
+        }
     }
 }
