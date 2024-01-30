d4f83a3dd4cb9efcdac30e005773d7599f1f0dfa
IdaDanuartha <danuart21@gmail.com>
2024-01-30 10:46:53 +0800

    update route in web.php

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::middleware(['auth'])->group(function() {
     Route::get('dashboard', DashboardController::class)->name("dashboard");        
     Route::resource('users', UserController::class);    
     Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('profile/update', [ProfileController::class, 'update'])->name('profile.update');
 });
 
 Route::fallback(function() {
