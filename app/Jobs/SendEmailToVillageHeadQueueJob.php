fcc33a7bc212d457dad43811bad072b76a0077d5
Ida Danuartha <danuart21@gmail.com>
2024-02-02 14:03:46 +0700

    add queue jobs

--- app/Jobs/SendEmailToVillageHeadQueueJob.php
+++ app/Jobs/SendEmailToVillageHeadQueueJob.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Mail\SendLetterMail;
+use App\Mail\SendLetterToVillageHead;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -11,20 +11,22 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-class SendEmailQueueJob implements ShouldQueue
+class SendEmailToVillageHeadQueueJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $send_mail;
     protected User $user;
+    protected $letter_code;
   
     /**
      * Create a new job instance.
      */
-    public function __construct($send_mail, User $user)
+    public function __construct($send_mail, User $user, $letter_code)
     {
         $this->send_mail = $send_mail;
         $this->user = $user;
+        $this->letter_code = $letter_code;
     }
   
     /**
@@ -32,7 +34,7 @@ class SendEmailQueueJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $email = new SendLetterMail($this->user);
+        $email = new SendLetterToVillageHead($this->user, $this->letter_code);
         Mail::to($this->send_mail)->send($email);
     }
 }
