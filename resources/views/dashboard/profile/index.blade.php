a43a21ed1e8b2f440a8e9860e94baec420a274d4
Ida Danuartha <danuart21@gmail.com>
2024-02-04 15:40:31 +0800

    fix bug in profile view

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -3,10 +3,12 @@
 @section('main')
 <div class="mt-[20px] p-0 flex gap-5 lg:flex-row flex-col">
     <div class="table-wrapper p-[18px] w-full h-fit md:max-w-[300px]">
-        @if (auth()->user()->authenticatable->profile_image ?? auth()->user()->authenticatable->citizent->profile_image)
-            <img src="{{ asset('uploads/users/' . auth()->user()->authenticatable->profile_image ?? auth()->user()->authenticatable->citizent->profile_image) }}" alt="Profile Image" class="rounded w-full aspect-square object-cover object-center h-auto"/>
+        @if (isset(auth()->user()->authenticatable->profile_image))
+            <img src="{{ asset('uploads/users/' . auth()->user()->authenticatable->profile_image) }}" alt="Profile Image" class="rounded w-full brightness-50 edit-profile-preview-img aspect-square object-cover object-center h-auto"/>
+        @elseif(isset(auth()->user()->authenticatable->citizent->profile_image))
+            <img src="{{ asset('uploads/users/' . auth()->user()->authenticatable->citizent->profile_image) }}" alt="Profile Image" class="rounded w-full brightness-50 edit-profile-preview-img aspect-square object-cover object-center h-auto"/>
         @else
-            <img src="{{ asset('assets/img/avatars/1.png') }}" alt="Profile Image" class="rounded w-full aspect-square object-cover object-center h-auto"/>
+            <img src="{{ asset('assets/img/avatars/1.png') }}" alt="Profile Image" class="edit-profile-preview-img rounded w-full aspect-square object-cover object-center h-auto brightness-50"/>
         @endif
     </div>
     <div class="table-wrapper">
@@ -52,19 +54,19 @@
                 </div>
                 <div class="col-md-6 col-12 mb-4 flex flex-col">
                     <label class="text-second">Jenis Kelamin</label>
-                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->gender ?? auth()->user()->authenticatable->citizent->gender->label() }}" readonly />
+                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->gender->label() ?? auth()->user()->authenticatable->citizent->gender->label() }}" readonly />
                 </div>
                 <div class="col-md-6 col-12 mb-4 flex flex-col">
                     <label class="text-second">Golongan Darah</label>
-                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->blood_group ?? auth()->user()->authenticatable->citizent->blood_group->label() }}" readonly />
+                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->blood_group->label() ?? auth()->user()->authenticatable->citizent->blood_group->label() }}" readonly />
                 </div>
                 <div class="col-md-6 col-12 mb-4 flex flex-col">
                     <label class="text-second">Agama</label>
-                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->religion ?? auth()->user()->authenticatable->citizent->religion->label() }}" readonly />
+                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->religion->label() ?? auth()->user()->authenticatable->citizent->religion->label() }}" readonly />
                 </div>
                 <div class="col-md-6 col-12 mb-4 flex flex-col">
                     <label class="text-second">Status Pernikahan</label>
-                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->marital_status ?? auth()->user()->authenticatable->citizent->marital_status->label() }}" readonly />
+                    <input type="text" class="input-crud" value="{{ auth()->user()->authenticatable->marital_status->label() ?? auth()->user()->authenticatable->citizent->marital_status->label() }}" readonly />
                 </div>
             @endif
             <div class="col-12">
