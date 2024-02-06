c9cc25d83d3298d39bec78501cd319de51cec048
IdaDanuartha <danuart21@gmail.com>
2024-02-06 16:49:51 +0800

    update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,52 +14,215 @@ class DashboardController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function getChartData()
     {
-        // Get Data This Year
-		$letterData = Letter::select(DB::raw("COUNT(*) as count"))
-                                      ->whereYear("created_at", date('Y'))				
-                                      ->groupBy(DB::raw("Month(created_at)"))
-                                      ->pluck("count");
+        if(auth()->user()->role === Role::ENVIRONMENTAL_HEAD) {
+            // Get Data This Year
+            $letterData = Letter::select(DB::raw("COUNT(*) as count"))
+                    ->whereYear("created_at", date('Y'))
+                    ->where('is_published', 1)				
+                    ->groupBy(DB::raw("Month(created_at)"))
+                    ->pluck("count");
 
-        $months = Letter::select(DB::raw("Month(created_at) as month"))
+            $months = Letter::select(DB::raw("Month(created_at) as month"))
                 ->whereYear('created_at', date('Y'))
+                ->where('is_published', 1)
                 ->groupBy(DB::raw("Month(created_at)"))
                 ->pluck("month");
 
-        $letter_yearly = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
-        foreach ($months as $index => $month) {
-            $letter_yearly[$month - 1] = $letterData[$index];
-        }
-
-        // Get Data This Week
-        $letter_weekly = [0, 0, 0, 0, 0, 0, 0];
-
-        foreach ($letter_weekly as $key => $item) {
+            $letter_yearly = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+            foreach ($months as $index => $month) {
+                $letter_yearly[$month - 1] = $letterData[$index];
+            }
+    
+            // Get Data This Week
+            $letter_weekly = [0, 0, 0, 0, 0, 0, 0];
+    
+            foreach ($letter_weekly as $key => $item) {
                 $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
-                ->whereDate('created_at', [Carbon::now()->startOfWeek()->addDays($key)->format('Y-m-d')])
-                ->groupBy(DB::raw("Date(created_at)"))
-                ->pluck("count")
-                ->toArray();
-
+                                                ->whereDate('created_at', [Carbon::now()->startOfWeek()->addDays($key)->format('Y-m-d')])
+                                                ->where('is_published', 1)
+                                                ->groupBy(DB::raw("Date(created_at)"))
+                                                ->pluck("count")
+                                                ->toArray();
+    
                 $letter_weekly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
-        }	
-
-        // Get Data This Month
-        $letter_monthly = [0, 0, 0, 0];
-
-        foreach ($letter_monthly as $key => $item) {
-            $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+            }	
+    
+            // Get Data This Month
+            $letter_monthly = [0, 0, 0, 0];
+    
+            foreach ($letter_monthly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
                 ->whereBetween('created_at', [
-                        Carbon::now()->startOfMonth()->addWeeks($key)->startOfWeek(),
-                        Carbon::now()->startOfMonth()->addWeeks($key)->endOfWeek(),
+                    Carbon::now()->startOfMonth()->addWeeks($key)->startOfWeek(),
+                    Carbon::now()->startOfMonth()->addWeeks($key)->endOfWeek(),
                 ])
+                ->where('is_published', 1)
                 ->groupBy(DB::raw("Week(created_at)"))
                 ->pluck("count")
                 ->toArray();
+    
+                $letter_monthly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }
+        } else if(auth()->user()->role === Role::SECTION_HEAD) {
+            // Get Data This Year
+            $letterData = Letter::select(DB::raw("COUNT(*) as count"))
+                    ->whereYear("created_at", date('Y'))
+                    ->where('approved_by_environmental_head', 1)				
+                    ->groupBy(DB::raw("Month(created_at)"))
+                    ->pluck("count");
 
-            $letter_monthly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
-        }	
+            $months = Letter::select(DB::raw("Month(created_at) as month"))
+                ->whereYear('created_at', date('Y'))
+                ->where('approved_by_environmental_head', 1)
+                ->groupBy(DB::raw("Month(created_at)"))
+                ->pluck("month");
+            
+            $letter_yearly = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+            foreach ($months as $index => $month) {
+                $letter_yearly[$month - 1] = $letterData[$index];
+            }
+    
+            // Get Data This Week
+            $letter_weekly = [0, 0, 0, 0, 0, 0, 0];
+    
+            foreach ($letter_weekly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                                                ->whereDate('created_at', [Carbon::now()->startOfWeek()->addDays($key)->format('Y-m-d')])
+                                                ->where('approved_by_environmental_head', 1)
+                                                ->groupBy(DB::raw("Date(created_at)"))
+                                                ->pluck("count")
+                                                ->toArray();
+    
+                $letter_weekly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }	
+    
+            // Get Data This Month
+            $letter_monthly = [0, 0, 0, 0];
+    
+            foreach ($letter_monthly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                ->whereBetween('created_at', [
+                    Carbon::now()->startOfMonth()->addWeeks($key)->startOfWeek(),
+                    Carbon::now()->startOfMonth()->addWeeks($key)->endOfWeek(),
+                ])
+                ->where('approved_by_environmental_head', 1)
+                ->groupBy(DB::raw("Week(created_at)"))
+                ->pluck("count")
+                ->toArray();
+    
+                $letter_monthly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }
+        } else if(auth()->user()->role === Role::VILLAGE_HEAD) {
+            // Get Data This Year
+            $letterData = Letter::select(DB::raw("COUNT(*) as count"))
+                    ->whereYear("created_at", date('Y'))
+                    ->where('approved_by_section_head', 1)				
+                    ->groupBy(DB::raw("Month(created_at)"))
+                    ->pluck("count");
+
+            $months = Letter::select(DB::raw("Month(created_at) as month"))
+                ->whereYear('created_at', date('Y'))
+                ->where('approved_by_section_head', 1)
+                ->groupBy(DB::raw("Month(created_at)"))
+                ->pluck("month");
+            
+            $letter_yearly = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+            foreach ($months as $index => $month) {
+                $letter_yearly[$month - 1] = $letterData[$index];
+            }
+    
+            // Get Data This Week
+            $letter_weekly = [0, 0, 0, 0, 0, 0, 0];
+    
+            foreach ($letter_weekly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                                                ->whereDate('created_at', [Carbon::now()->startOfWeek()->addDays($key)->format('Y-m-d')])
+                                                ->where('approved_by_section_head', 1)
+                                                ->groupBy(DB::raw("Date(created_at)"))
+                                                ->pluck("count")
+                                                ->toArray();
+    
+                $letter_weekly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }	
+    
+            // Get Data This Month
+            $letter_monthly = [0, 0, 0, 0];
+    
+            foreach ($letter_monthly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                ->whereBetween('created_at', [
+                    Carbon::now()->startOfMonth()->addWeeks($key)->startOfWeek(),
+                    Carbon::now()->startOfMonth()->addWeeks($key)->endOfWeek(),
+                ])
+                ->where('approved_by_section_head', 1)
+                ->groupBy(DB::raw("Week(created_at)"))
+                ->pluck("count")
+                ->toArray();
+    
+                $letter_monthly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }
+        } else {
+            // Get Data This Year
+            $letterData = Letter::select(DB::raw("COUNT(*) as count"))
+                    ->whereYear("created_at", date('Y'))                    				
+                    ->groupBy(DB::raw("Month(created_at)"))
+                    ->pluck("count");
+
+            $months = Letter::select(DB::raw("Month(created_at) as month"))
+                ->whereYear('created_at', date('Y'))                
+                ->groupBy(DB::raw("Month(created_at)"))
+                ->pluck("month");
+
+            $letter_yearly = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+            foreach ($months as $index => $month) {
+                $letter_yearly[$month - 1] = $letterData[$index];
+            }
+    
+            // Get Data This Week
+            $letter_weekly = [0, 0, 0, 0, 0, 0, 0];
+    
+            foreach ($letter_weekly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                                                ->whereDate('created_at', [Carbon::now()->startOfWeek()->addDays($key)->format('Y-m-d')])                                                
+                                                ->groupBy(DB::raw("Date(created_at)"))
+                                                ->pluck("count")
+                                                ->toArray();
+    
+                $letter_weekly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }	
+    
+            // Get Data This Month
+            $letter_monthly = [0, 0, 0, 0];
+    
+            foreach ($letter_monthly as $key => $item) {
+                $getLetterCurrentWeek = Letter::select(DB::raw("COUNT(*) as count"))
+                ->whereBetween('created_at', [
+                    Carbon::now()->startOfMonth()->addWeeks($key)->startOfWeek(),
+                    Carbon::now()->startOfMonth()->addWeeks($key)->endOfWeek(),
+                ])
+                ->where('approved_by_village_head', 1)
+                ->groupBy(DB::raw("Week(created_at)"))
+                ->pluck("count")
+                ->toArray();
+    
+                $letter_monthly[$key] = count($getLetterCurrentWeek) ? $getLetterCurrentWeek[0] : 0;
+            }
+        }
+        
+        return [
+            $letter_yearly,
+            $letter_weekly,
+            $letter_monthly,
+        ];
+    }
+
+    public function __invoke(Request $request)
+    {
+        $letter_yearly = $this->getChartData()[0];
+        $letter_weekly = $this->getChartData()[1];
+        $letter_monthly = $this->getChartData()[2];
 
         $total_citizents = Citizent::count();
         $total_letters = Letter::count();
@@ -78,19 +241,20 @@ class DashboardController extends Controller
             $total_letters = Letter::where('is_published', 1)->count();
         } else if(auth()->user()->role === Role::SECTION_HEAD) {
             $total_letters_approved = Letter::where('section_head_id', auth()->user()->authenticatable->id)
+                                            ->where('approved_by_environmental_head', 1)
                                             ->count();
-            $total_letters_not_approved = Letter::where('section_head_id', auth()->user()->authenticatable->id)
-                                                ->where('approved_by_section_head', 0)
+            $total_letters_not_approved = Letter::where('approved_by_section_head', 0)
+                                                ->where('approved_by_environmental_head', 1)
                                                 ->count();
-            $total_letters = Letter::where('section_head_id', "!=", null)->count();
+            $total_letters = Letter::where('approved_by_environmental_head', 1)->count();
         } else if(auth()->user()->role === Role::VILLAGE_HEAD) {
             $total_letters_approved = Letter::where('village_head_id', auth()->user()->authenticatable->id)
-                                            ->where('approved_by_village_head', 1)
+                                            ->where('approved_by_section_head', 1)
                                             ->count();
-            $total_letters_not_approved = Letter::where('village_head_id', auth()->user()->authenticatable->id)
-                                                ->where('approved_by_village_head', 0)
+            $total_letters_not_approved = Letter::where('approved_by_village_head', 0)
+                                                ->where('approved_by_section_head', 1)
                                                 ->count();
-            $total_letters = Letter::where('village_head_id', "!=", null)->count();
+            $total_letters = Letter::where('approved_by_section_head', 1)->count();
         } else {
             $total_letters_approved = Letter::where('citizent_id', auth()->user()->authenticatable->id)
                                             ->where('approved_by_village_head', 1)
