ac4a2712a9287b45e4cea2daab329edc40af85a3
Ida Danuartha <danuart21@gmail.com>
2024-02-10 22:39:47 +0800

    add new model

---
+++ app/Models/SuperAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class SuperAdmin extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user(): MorphOne
+	{
+		return $this->morphOne(User::class, 'authenticatable');
+	}
+}
