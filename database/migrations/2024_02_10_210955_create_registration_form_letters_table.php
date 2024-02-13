027797b96ece0f72fc788ac2fca8aa230a4b02ee
Ida Danuartha <danuart21@gmail.com>
2024-02-13 13:09:32 +0800

    update migration registration form

--- database/migrations/2024_02_10_210955_create_registration_form_letters_table.php
+++ database/migrations/2024_02_10_210955_create_registration_form_letters_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Sk;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('registration_form_letters', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Sk::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string("time_period");
+            $table->string("reason");
             $table->timestamps();
         });
     }
