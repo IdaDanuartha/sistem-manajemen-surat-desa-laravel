792442d125b8cef661b0e35511754763d4bb1054
IdaDanuartha <danuart21@gmail.com>
2024-02-15 22:41:41 +0800

    little update

--- resources/views/dashboard/letters/sk-move/crud/detail.blade.php
+++ resources/views/dashboard/letters/sk-move/crud/detail.blade.php
@@ -162,7 +162,7 @@
 					</label>
 				</div>
 			@endif	
-			@if (auth()->user()->isSectionHead())
+			@if (auth()->user()->isSectionHead() || auth()->user()->isCitizent())
 			<div class="col-span-12 flex flex-col">
 				<label for="signature_image" class="text-second mb-1">Tanda Tangan Elektronik</label>
 				@if (auth()->user()->signature_image)
