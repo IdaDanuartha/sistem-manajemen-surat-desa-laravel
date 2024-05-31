b2b6036c9accc721cb30c206c59e6585c510a0c9
IdaDanuartha <danuart21@gmail.com>
2024-05-31 12:08:01 +0800

    dropSoftDeletes when migrate rollback

--- database/migrations/2024_05_28_162525_add_deleted_at_column_to_table.php
+++ database/migrations/2024_05_28_162525_add_deleted_at_column_to_table.php
@@ -94,7 +94,79 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sks', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_birth_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_marry_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_marital_status_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sktu_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sktm_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_name_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_subsidized_housing_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_move_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_traveling_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_residence_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_land_price_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_parent_income_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_die_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('parental_permission_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_grant_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('diesel_purchase_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_domicile_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('registration_form_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('tree_felling_letters', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_power_attorneys', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('inheritance_geneologies', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_heirs', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+        Schema::table('sk_inheritance_distributions', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });    
     }
 };
